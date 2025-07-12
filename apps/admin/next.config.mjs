b24712a1a204cd2587ff85f36dce42b8caa70d1c
Joseph Itakala <josephitakala18@gmail.com>
2025-07-12 21:16:57 +0200

    fix: error on deployment on Vercel

--- apps/admin/next.config.mjs
+++ apps/admin/next.config.mjs
@@ -1,6 +1,15 @@
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   transpilePackages: ["@workspace/ui", "database"],
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins.push(new PrismaPlugin());
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
